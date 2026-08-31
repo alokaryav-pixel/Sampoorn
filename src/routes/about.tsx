605dfb78051e50a8f457621a154692f90e872eb4
Alok Verma <alok.aryav@gmail.com>
2026-08-31 21:29:19 +0530

    tryin degregation in about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -113,6 +113,7 @@ function About() {
 
       <Section className="!pt-0">
         <SectionHeading eyebrow="Leadership" title="A senior team," serifPart="present in the work." />
+        <SectionHeading title="Core Team," />
         <div className="mt-14 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {[
             {
